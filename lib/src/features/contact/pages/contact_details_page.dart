16f7c295f93b4991b82948365e79131a56bb5d99
Archana CS <107298198+Archana777CS@users.noreply.github.com>
2022-06-26 14:40:43 +0530

    Update contact_details_page.dart

--- lib/src/features/contact/pages/contact_details_page.dart
+++ lib/src/features/contact/pages/contact_details_page.dart
@@ -1,12 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:json_cubit/src/core/utils/link_open.dart';
 import 'package:json_cubit/src/core/utils/utility_launcher.dart';
 import 'package:json_cubit/src/features/contact/models/contact_model.dart';
 
 class ContactDetailsPage extends StatefulWidget {
-final ContactModel contactDetails;
-  const ContactDetailsPage({required this.contactDetails}) ;
+  final ContactModel contactDetails;
+  // ignore: use_key_in_widget_constructors
+  const ContactDetailsPage({required this.contactDetails});
 
   @override
   State<ContactDetailsPage> createState() => _ContactDetailsPageState();
@@ -17,53 +17,93 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contact Details"),
+        title: Text(widget.contactDetails.name ?? "",
+            style: const TextStyle(
+                fontSize: 28,
+                color: Colors.white70,
+                fontWeight: FontWeight.w700)),
       ),
       body: Center(
         child: Column(
           children: [
-            Image.network(widget.contactDetails.photo??""),
+            Image.network(widget.contactDetails.photo ?? ""),
             Text(
-               widget.contactDetails.name??"",
-              style: TextStyle(
+              widget.contactDetails.name ?? "",
+              style: const TextStyle(
                   fontSize: 24,
                   color: Colors.black,
                   fontWeight: FontWeight.bold),
             ),
-            Text(
-               widget.contactDetails.address??"",
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
+            Card(
+              borderOnForeground: true,
+              child: Text(
+                widget.contactDetails.address ?? "",
+                style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-            ListTile(
-              onTap: () {
-                LauncherUtility.makeCall( widget.contactDetails.contact??"");
-              },
-              title: Text(
-                widget.contactDetails.contact?? "",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.green,
-                  fontWeight: FontWeight.normal,
+            Card(
+              child: ListTile(
+                onTap: () {
+                  LauncherUtility.makeCall(widget.contactDetails.contact ?? "");
+                },
+                title: Center(
+                  child: Row(
+                    children: [
+                      const Card(
+                        child: Center(
+                          child: Icon(
+                            Icons.phone,
+                            color: Colors.green,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.contactDetails.contact ?? "",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.green,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            ListTile(
-              title: Text(
-                widget.contactDetails.url?? "",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w200),
+           Card(child:
+           ListTile(
+              title: Center(
+                child: Row(
+                  children: [
+                    const Card(
+                      child: Center(
+                        child: Icon(
+                          Icons.web,
+                          color: Colors.blue,
+                          size: 25,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      widget.contactDetails.url ?? "",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
               ),
               onTap: () {
-                LinkOpen.openLink(widget.contactDetails.url?? "");
+                LinkOpen.openLink(widget.contactDetails.url ?? "");
               },
-            ),
+            ),),
           ],
         ),
       ),
